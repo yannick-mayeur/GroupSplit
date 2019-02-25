eaa4e3b47112819354566f6f1d7c1cd435f7764c
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-02-25 09:08:03 +0100

    Add function that returns notees with certain mark

--- programme.py
+++ programme.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 donneeBrut = np.genfromtxt('preferences.csv', dtype=str, delimiter=',')
 
-for ligne in donneeBrut:
-  for item in ligne:
-    print(item)
+def getNoteeWithMark(noter, mark):
+    """Return a list of Notees that Noter noted with mark"""
+    i = -1
+    isFound = False
+    while i < (len(donneeBrut[0])-1) and not isFound:
+        i += 1
+        if noter ==  donneeBrut[i][0]:
+            isFound = True
+    result = []
+    j = -1
+    isFound = False
+    while j < (len(donneeBrut)-1) and not isFound:
+        j += 1
+        if mark == donneeBrut[i][j]:
+            result.append(donneeBrut[0][j])
+    return result
+
+
+print(getNoteeWithMark("21708799", "I"))
